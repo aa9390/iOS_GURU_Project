2c5f36c9fe15a0458b9ac2b6ba7ce2d9fe018422
Soyeon Kim <ese2003@naver.com>
2018-07-06 16:29:44 +0900

    initial commit(storyboard)

---
+++ guru_project_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  guru_project_2
+//
+//  Created by swuad_29 on 2018. 7. 6..
+//  Copyright © 2018년 swuad_29. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
